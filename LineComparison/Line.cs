af453d638e399814ba3c18f90df1a34612f859bd
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-02 13:12:32 +0530

    [Harshitha] Creating a class for comparing two lines

---
+++ LineComparison/Line.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    class Line
+    {
+        public string name;
+        public int x1, x2, y1, y2;
+        public double lengthOfTheLine;
+        public Line(string name)
+        {
+            this.name = name;
+            this.x1 = 0;
+            this.x2 = 0;
+            this.y1 = 0;
+            this.y2 = 0;
+            this.lengthOfTheLine = 0;
+        }
+    }
+}
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -7,25 +7,27 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-            Console.Write("Enter the value x1 :");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the value y1 :");
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the value x2 :");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the value y2 :");
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the value x3 :");
-            int x3 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the value y3 :");
-            int y3 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the value x4 :");
-            int x4 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the value y4 :");
-            int y4 = Convert.ToInt32(Console.ReadLine());
-            double length_in_line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            double length_in_line2 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            int line_comparison = length_in_line1.CompareTo(length_in_line2);
+            Line line1 = new Line("line1");
+            Line line2 = new Line("line2");
+            Console.Write("Enter the value x1 for line1 :");
+            line1.x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value y1 for line1 :");
+            line1.y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value x2 for line1 :");
+            line1.x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value y2 for line1 :");
+            line1.y2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value x1 for line2:");
+            line2.x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value y1 for line2 :");
+            line2.y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value x2 for line2 :");
+            line2.x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value y2 for line2 :");
+            line2.y2 = Convert.ToInt32(Console.ReadLine());
+            line1.lengthOfTheLine = Math.Sqrt(Math.Pow(line1.x2 - line1.x1, 2) + Math.Pow(line1.y2 - line1.y1, 2));
+            line2.lengthOfTheLine = Math.Sqrt(Math.Pow(line2.x2 - line2.x1, 2) + Math.Pow(line2.y2 - line2.y1, 2)) ;
+            int line_comparison = line1.lengthOfTheLine.CompareTo(line2.lengthOfTheLine);
             if(line_comparison==0)
             {
                 Console.WriteLine("Lines are equal in length");
